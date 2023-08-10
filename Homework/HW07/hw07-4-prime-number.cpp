0ce1cf69d65a9b2a1e574390bafac60cb9e66c7d
Jpwrbd <jpwrbd456@gmail.com>
2023-08-10 21:25:46 +0700

    up

--- Homework/HW07/hw07-4-prime-number.cpp
+++ Homework/HW07/hw07-4-prime-number.cpp
@@ -1,7 +1,7 @@
 /*
     รับค่าจากผู้ใช้จำนวน 1 ค่า และให้แสดงจำนวนเฉพาะตั้งแต่ค่าแรก จนถึงค่าที่ผู้ใช้กรอก โดยแสดงจำนวนเฉพาะจาก "มาก ไปหา น้อย"
     (โจทย์ข้อนี้ให้ใช้ Do While Loop เท่านั้น)
-    
+
     Test case:
         Enter number :
             10
@@ -14,3 +14,46 @@
     Output:
         97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
 */
+#include <stdio.h>
+
+int main()
+{
+    int limit;
+
+    printf("Enter number: ");
+    scanf("%d", &limit);
+
+    int num = limit;
+    int is_prime = 1; // Assuming num is prime
+
+    do
+    {
+        if (num <= 1)
+        {
+            is_prime = 0;
+        }
+        else
+        {
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    is_prime = 0;
+                    break;
+                }
+            }
+        }
+
+        if (is_prime)
+        {
+            printf("%d ", num);
+        }
+
+        num--;
+        is_prime = 1; // Reset is_prime for the next iteration
+    } while (num >= 2);
+
+    printf("\n");
+
+    return 0;
+}
